78142a85790ff22140d956b9ec9fec3fb6d9e3fe
Shindou Mihou <kokocrunchsh@gmail.com>
2022-07-01 23:14:23 +0800

    Fixes clipping behind image for blocks.

--- src/lib/components/Block.svelte
+++ src/lib/components/Block.svelte
@@ -2,7 +2,7 @@
   import { fade } from "svelte/transition";
 </script>
 
-<div class="fixed top-5 w-full right-[0.5%] left-[0.6%] md:left-[50%] md:right-[50%]" transition:fade>
+<div class="fixed top-5 w-full right-[0.5%] left-[0.6%] md:left-[50%] md:right-[50%] z-50" transition:fade>
   <div class="flex flex-row justify-between items-center w-[50%] bg-green-500 shadow p-3 m-auto mb-4 text-white rounded-lg">
     <div class="flex flex-row gap-2 items-center">
       <slot></slot>
--- src/lib/components/ErrorBlock.svelte
+++ src/lib/components/ErrorBlock.svelte
@@ -4,7 +4,7 @@
     export let message: string;
 </script>
 
-<div id="error-block" class="flex flex-row justify-between items-center w-full bg-red-200 p-2 mb-4 text-black rounded-sm">
+<div id="error-block" class="flex flex-row justify-between items-center w-full bg-red-200 p-2 mb-4 text-black rounded-sm z-50">
     <div class="flex flex-row gap-2 items-center">
       <Icon src={Annotation} solid class="text-red-500 w-[1.10rem] flex-shrink-0"></Icon>
       <p class="text-xs">{message}</p>
